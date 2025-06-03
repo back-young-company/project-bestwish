b47234a0d578979c41e18aded8d9dd2f4289ca13
LeeSuHyeon <tngus0673@naver.com>
2025-06-03 23:20:11 +0900

    feat: #1 - Hex -> UIColor 변환 메서드 구현

---
+++ BestWish/Presentation/Extension/UIColor+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+Extension.swift
+//  BestWish
+//
+//  Created by 이수현 on 6/3/25.
+//
+
+import UIKit
+
+extension UIColor {
+    // Hex 문자열을 UIColor로 변환하는 메서드
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        // Hex 문자열 길이에 따라 RGB 값을 추출
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        // UIColor 초기화
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+
+extension UIColor {
+    static let main = UIColor(hex: "#7F6CFF")
+}
+
