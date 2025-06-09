f30ef6bd64fd166e9657b8326fea45e7a34df6ad
LeeSuHyeon <tngus0673@naver.com>
2025-06-09 11:46:14 +0900

    refactor: #14: addArrangedSubviews 수정

--- BestWish/Presentation/Extension/UIStackView+Extension.swift
+++ BestWish/Presentation/Extension/UIStackView+Extension.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIStackView {
-    func addArrangedSubviews(views: UIView...) {
+    func addArrangedSubviews(_ views: UIView...) {
         for view in views {
             self.addArrangedSubview(view)
         }
