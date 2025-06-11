abe511e06d1550e41f97cbe0023245cf021a5aa6
LeeSuHyeon <tngus0673@naver.com>
2025-06-11 12:06:08 +0900

    feature: #23 - 프로필 Sheet 구현

---
+++ BestWish/Presentation/Scene/ProfileSheet/Suview/ProfileSheetCell.swift
@@ -0,0 +1,60 @@
+//
+//  ProfileSheetCell.swift
+//  BestWish
+//
+//  Created by 이수현 on 6/10/25.
+//
+
+import UIKit
+
+final class ProfileSheetCell: UICollectionViewCell, ReuseIdentifier {
+    private let imageView = UIImageView()
+
+    override var isSelected: Bool {
+        didSet {
+            imageView.layer.borderWidth = isSelected ? 3 : 0
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setView()
+    }
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+
+    func configure(imageName: String) {
+        imageView.image = .init(named: imageName)
+    }
+}
+
+private extension ProfileSheetCell {
+    func setView() {
+        setAttributes()
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setAttributes() {
+        imageView.do {
+            $0.contentMode = .scaleAspectFit
+            $0.layer.cornerRadius = CGFloat(80).fitHeight / 2
+            $0.clipsToBounds = true
+            $0.layer.borderColor = UIColor.primary300?.cgColor
+        }
+    }
+
+    func setHierarchy() {
+        self.contentView.addSubview(imageView)
+    }
+
+    func setConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
