7495c721e52c1c0a16d09b3337ae62f7b67cdad6
Yimkeul <leesjun29@gmail.com>
2025-06-12 11:55:40 +0900

    chore: #21 - text 추가

--- BestWish/Presentation/Scene/Onboarding/ViewController/Component/OnboardingText.swift
+++ BestWish/Presentation/Scene/Onboarding/ViewController/Component/OnboardingText.swift
@@ -12,6 +12,7 @@ enum OnboardingText {
     case firstDesc
     case secondTitle
     case secondDesc
+    case secondCaution
 
     var value: String {
         switch self {
@@ -19,6 +20,7 @@ enum OnboardingText {
         case .firstDesc: return "당신이 누군지 알려주세요!"
         case .secondTitle: return "프로필을 설정해주세요."
         case .secondDesc: return "나를 표현해봐요!"
+        case .secondCaution: return "* 2자이상 10자 이내의 한글, 영문, 숫자 입력 가능합니다."
         }
     }
 }
