37b504b80c8bde058d3b4eb1a183e74b9de8f959
Yimkeul <leesjun29@gmail.com>
2025-06-12 16:01:31 +0900

    feat: #21 - Onboarding ViewController 수정

--- BestWish/Presentation/Scene/Onboarding/ViewController/OnboardingViewController.swift
+++ BestWish/Presentation/Scene/Onboarding/ViewController/OnboardingViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 import RxSwift
 
 final class OnboardingViewController: UIViewController {
-    // MARK: - Properties
     private let firstView = OnboardingFirstView()
     private let secondView = OnboardingSecondView()
     private let viewModel: OnboardingViewModel
     private let disposeBag = DisposeBag()
+    private let onboardingViews: [UIView]
 
-    // MARK: - Initializer, Deinit, requiered
     init(viewModel: OnboardingViewModel) {
         self.viewModel = viewModel
+        self.onboardingViews = [firstView, secondView]
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -26,51 +26,105 @@ final class OnboardingViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // MARK: - View Life Cycle
     override func loadView() {
-        view = secondView
+        view = firstView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         bindView()
         bindViewModel()
+        bindPageButton()
     }
 
-    // MARK: - BindView
+
     private func bindView() {
-        firstView.genderSelection.selectedGender
-            .subscribe(onNext: { gender in
-            switch gender {
-            case .male:
-                print("남 선택됨")
-            case .female:
-                print("여 선택됨")
-            case .nothing:
-                print("선택 안 함")
-            default:
-                break
-            }
-        })
+        bindFirstView()
+        bindSecondView()
+        bindPageButton()
+    }
+
+    private func bindViewModel() {
+        viewModel.state.currentPage
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, page in
+            self.view = owner.onboardingViews[page]
+        }
             .disposed(by: disposeBag)
 
+        viewModel.state.userInput
+            .bind(with: self) { owner, userInput in
+            owner.secondView.configure(input: userInput)
+        }
+            .disposed(by: disposeBag)
+
+        viewModel.state.userInput
+            .map { input in
+            return input.gender != nil && input.birth != nil
+        }
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, isValid in
+            owner.firstView.nextPageButton.isEnabled = isValid
+            owner.firstView.configure(isValid)
+        }
+            .disposed(by: disposeBag)
+
+        Observable
+            .combineLatest(
+            viewModel.state.userInput.map { $0.nickname != nil },
+            viewModel.state.isValidNickname
+        ) { hasNick, isValid in
+            hasNick && isValid
+        }
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, enabled in
+            owner.secondView.completeButton.isEnabled = enabled
+            owner.secondView.configure(enabled)
+        }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension OnboardingViewController: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        // 모달이 내려간 시점에 원래 색으로 복구
+        firstView.birthSelection.dateButton.layer.borderColor = UIColor.gray200?.cgColor
+    }
+}
+
+private extension OnboardingViewController {
+    private func bindFirstView() {
+        // 성별 바인딩
+        firstView.genderSelection.selectedGender
+            .skip(1)
+            .subscribe(with: self) { owner, gender in
+            owner.viewModel.action.onNext(.selectedGender(gender ?? .nothing))
+        }
+            .disposed(by: disposeBag)
+
+        // 생일 바인딩
         firstView.birthSelection.dateButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-            guard let self else { return }
-            self.firstView.birthSelection.dateButton.layer.borderColor = UIColor.primary300?.cgColor
+            .subscribe(with: self) { owner, _ in
+            owner.firstView.birthSelection.dateButton.layer.borderColor = UIColor.primary300?.cgColor
             let sheetVC = DatePickerBottomSheetViewController()
             sheetVC.presentationController?.delegate = self
             // 선택된 날짜 콜백
             sheetVC.onDateSelected = { date in
                 let formatter = DateFormatter()
+                // 현재 한국시간으로만 설정
                 formatter.locale = Locale(identifier: "ko_KR")
                 formatter.dateFormat = "yyyy.MM.dd"
                 let title = formatter.string(from: date)
                 self.firstView.birthSelection.dateButton.setTitle(title, for: .normal)
 
-                self.dismiss(animated: false) {
+
+                self.dismiss(animated: true) {
                     self.firstView.birthSelection.dateButton.layer.borderColor = UIColor.gray200?.cgColor
+
                 }
+                owner.viewModel.action.onNext(.selectedBirth(date))
             }
 
             sheetVC.onCancel = {
@@ -81,36 +135,92 @@ final class OnboardingViewController: UIViewController {
 
             sheetVC.presentDatePickerSheet()
             self.present(sheetVC, animated: true)
-        })
+        }
             .disposed(by: disposeBag)
+    }
 
+    private func bindSecondView() {
+        // 프로필 사진 바인딩
         let tapGesture = UITapGestureRecognizer()
         secondView.profileImageView.addGestureRecognizer(tapGesture)
 
         tapGesture.rx.event
             .withLatestFrom(viewModel.state.userInput)
             .bind(with: self) { owner, userAccount in
-                let profileSheetVC = ProfileSheetViewController(selectedIndex: userAccount.profileImageIndex)
-                profileSheetVC.presentProfileSheet()
-                profileSheetVC.onComplete = { [weak self] selectedIndex in
-                    self?.viewModel.action.onNext(.selectedProfileIndex(selectedIndex))
-                }
-                owner.present(profileSheetVC, animated: true)
-            }.disposed(by: disposeBag)
-    }
-
-    private func bindViewModel() {
-        viewModel.state.userInput
-            .bind(with: self) { owner, userInput in
-                owner.secondView.configure(input: userInput)
+            let profileSheetVC = ProfileSheetViewController(selectedIndex: userAccount.profileImageIndex)
+            profileSheetVC.presentProfileSheet()
+            profileSheetVC.onComplete = { [weak self] selectedIndex in
+                self?.viewModel.action.onNext(.selectedProfileIndex(selectedIndex))
             }
-            .disposed(by: disposeBag)
-    }
-}
+            owner.present(profileSheetVC, animated: true)
+        }.disposed(by: disposeBag)
 
-extension OnboardingViewController: UIAdaptivePresentationControllerDelegate {
-    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-        // 모달이 내려간 시점에 원래 색으로 복구
-        firstView.birthSelection.dateButton.layer.borderColor = UIColor.gray200?.cgColor
+        // 닉네임 바인딩
+        /// 1) 텍스트 스트림
+        let textStream = secondView.nicknameStackView.textField.rx.text.orEmpty
+            .skip(2)
+
+        /// 2) 텍스트+유효성 스트림
+        let validationStream: Observable<(String, Bool)> = textStream
+            .map { text in
+            let isValid = NSPredicate(
+                format: "SELF MATCHES %@",
+                "^[가-힣A-Za-z0-9]{2,10}$"
+            ).evaluate(with: text)
+            return (text, isValid)
+        }
+            .distinctUntilChanged { prev, curr in
+            prev.0 == curr.0 && prev.1 == curr.1
+        }
+
+        /// 3) 구독
+        validationStream
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, data in
+            let (text, isValid) = data
+            owner.secondView.nicknameStackView.textField.layer.borderColor =
+                isValid ? UIColor.primary300?.cgColor // 허용 값
+            : UIColor.red0?.cgColor // 불허 값
+
+            owner.secondView.nicknameStackView.cautionLabel.textColor =
+                isValid ? .gray200 : .red0?
+
+                // 닉네임 유효성 검사 성공시 전달
+            owner.viewModel.action.onNext(.inputNicknameValid(isValid))
+
+            if isValid {
+                // 닉네임 유효성 검사 성공시 닉네임 전달
+                owner.viewModel.action.onNext(.inputNickname(text))
+            }
+        }
+            .disposed(by: disposeBag)
+
+    }
+
+    private func bindPageButton() {
+        firstView.nextPageButton.rx.tap
+            .asDriver()
+            .map { .nextPage }
+            .drive(viewModel.action)
+            .disposed(by: disposeBag)
+
+        secondView.prevButton.rx.tap
+            .asDriver()
+            .map { .prevPage }
+            .drive(viewModel.action)
+            .disposed(by: disposeBag)
+
+        // TODO: 메인화면으로 이동해야함.
+        secondView.completeButton.rx.tap
+            .withLatestFrom(viewModel.state.userInput)
+            .subscribe(onNext: { display in
+            print("Display:", display)
+            print("성별:", display.gender ?? "없음")
+            print("생일:", display.birth ?? "없음")
+            print("사진:", display.profileImageIndex)
+            print("닉네임:", display.nickname ?? "없음")
+
+        })
+            .disposed(by: disposeBag)
     }
 }
