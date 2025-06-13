9cf1a4b88a0c146c34e8a9f0d573b47ea18610df
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-13 11:50:34 +0900

    feat: #32 - 라벨 데이터 모델과 디스플레이로 정의
    - 모델은 실제 모델에서 추출한 데이터를 저장
    - 디스플레이는 뷰에서 표시할 데이터를 저장

---
+++ BestWish/Domain/Model/LabelData.swift
@@ -0,0 +1,14 @@
+//
+//  LabelData.swift
+//  BestWish
+//
+//  Created by Quarang on 6/13/25.
+//
+
+import Foundation
+
+// MARK: - 처음 CoreML 모델에서 추출한 데이터
+struct LabelData {
+    let label: String
+    let probability: String
+}
---
+++ BestWish/Presentation/Display/LabelDataDisplay.swift
@@ -0,0 +1,25 @@
+//
+//  LabelDataDisplay.swift
+//  BestWish
+//
+//  Created by Quarang on 6/13/25.
+//
+
+import Foundation
+
+// MARK: - 실제 뷰에서 사용할 데이터
+struct LabelDataDisplay {
+    let topCategory: String
+    let subCategory: String
+    let attributes: String
+    let probability: String
+}
+
+// MARK: - 데이터 매핑
+extension LabelDataDisplay {
+    static func convertToDisplay(from model: LabelData) -> LabelDataDisplay {
+        let labelArray = model.label.split(separator: ":").map { String($0) }
+        let (top, sub, attributes) = (labelArray[0], labelArray[1], labelArray[2])
+        return LabelDataDisplay(topCategory: top, subCategory: sub, attributes: attributes, probability: model.probability)
+    }
+}
