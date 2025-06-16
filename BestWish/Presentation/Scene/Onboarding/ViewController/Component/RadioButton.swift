afee7d09bc6c752fd401d0b923d972d79068e00a
Yimkeul <leesjun29@gmail.com>
2025-06-16 16:42:13 +0900

    chore: #45

--- BestWish/Presentation/Scene/Onboarding/ViewController/Component/RadioButton.swift
+++ BestWish/Presentation/Scene/Onboarding/ViewController/Component/RadioButton.swift
@@ -8,23 +8,12 @@
 import UIKit
 import Then
 
-// TODO: 버튼을 선택하고 데이트 피커를 띄었을때 회색 배경처리됨을 확인함. (수정필요)
 final class RadioButton: UIButton {
-    private let offImage: UIImage?
-    private let onImage: UIImage?
 
-    init(
-        title: String,
-        offImage: UIImage? = UIImage(named: "radioOff"),
-        onImage: UIImage? = UIImage(named: "radioOn")
-    ) {
-        self.offImage = offImage
-        self.onImage = onImage
+    init(title: String) {
         super.init(frame: .zero)
         setAttributes(title: title)
     }
-
-
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) 사용 불가")
@@ -46,9 +35,9 @@ final class RadioButton: UIButton {
                 return outgoing
             }
             $0.configuration = config
-            
-            $0.setImage(offImage, for: .normal)
-            $0.setImage(onImage, for: .selected)
+
+            $0.setImage(UIImage(named: "radioOff"), for: .normal)
+            $0.setImage(UIImage(named: "radioOn"), for: .selected)
         }
     }
 }
