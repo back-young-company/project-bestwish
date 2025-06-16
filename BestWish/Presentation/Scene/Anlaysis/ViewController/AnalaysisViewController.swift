c8480f600349949a6775f88fec6de6717bc7a22c
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-16 17:20:03 +0900

    feat: #32 - 이미지 분석 뷰 컨트롤러 이벤트 바인딩
    - 이벤트 방출 및 구독 작업

--- BestWish/Presentation/Scene/Anlaysis/ViewController/AnalaysisViewController.swift
+++ BestWish/Presentation/Scene/Anlaysis/ViewController/AnalaysisViewController.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
+
+// 비어 있는 키워드 예외처리를 위함
+let emptyKeyword = "해당 카테고리의 키워드를 인식할 수 없습니다."
 
 // MARK: - 이미지 분석 후 검색 뷰 컨트롤러
 final class AnalaysisViewController: UIViewController {
@@ -54,12 +59,12 @@ final class AnalaysisViewController: UIViewController {
                     return UICollectionReusableView()
                 }
                 
+                // 세그먼트 컨트롤 타이틀 데이터를 이벤트로 방출
                 headerView.getClassSegmentControl.rx.selectedSegmentIndex
                     .distinctUntilChanged()
-                    .filter { $0 != UISegmentedControl.noSegment } // prevent -1 crash
                     .bind(with: self) { owner, index in
                         let category = headerView.getClassSegmentControl.titleForSegment(at: index) ?? ""
-                        owner.viewModel.action.onNext(.currentString(category))
+                        owner.viewModel.action.onNext(.didSelectedSegmentControl(category: category))
                     }
                     .disposed(by: headerView.disposeBag)
                 return headerView
@@ -82,10 +87,46 @@ final class AnalaysisViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setDelegate()
         bindView()
     }
     
     private func bindView() {
+        // 모델 선택 시 이벤트 방출
+        analysisView.getCollectionView.rx.modelSelected(AnalysisItem.self)
+            .subscribe(with: self) { owner, item in
+                switch item {
+                case let .keyword(keyword):
+                    owner.viewModel.action.onNext(.didTapKeywordChip(keyword: keyword))
+                case let .attribute(attribute, _):
+                    guard attribute != emptyKeyword else { return }
+                    owner.viewModel.action.onNext(.didTapAttributeChip(attribute: attribute))
+                case let .platform(platform, _):
+                    owner.viewModel.action.onNext(.didTapPlatformChip(platform: platform))
+                }
+            }
+            .disposed(by: disposeBag)
         
+        // 라벨 데이터 컬렉션 뷰에 바인딩
+        viewModel.state.labelDatas
+            .bind(to: analysisView.getCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        // 서치바 이벤트 방출
+        analysisView.getSearchBar.rx.searchButtonClicked
+            .withLatestFrom(analysisView.getSearchBar.rx.text.orEmpty)
+            .subscribe(with: self) { owner, keyword in
+                owner.analysisView.getSearchBar.text = nil
+                owner.viewModel.action.onNext(.didSubmitSearchText(keyword: keyword))
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    /// 델리게이트 설정
+    private func setDelegate() {
+        analysisView.getCollectionView.register(KeywordCell.self, forCellWithReuseIdentifier: KeywordCell.identifier)
+        analysisView.getCollectionView.register(AttributeCell.self, forCellWithReuseIdentifier: AttributeCell.identifier)
+        analysisView.getCollectionView.register(PlatformCell.self, forCellWithReuseIdentifier: PlatformCell.identifier)
+        analysisView.getCollectionView.register(SegmentControlHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SegmentControlHeaderView.identifier)
     }
 }
