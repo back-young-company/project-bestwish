8d7d720e1feb5cde2ba2b1752dcf2cf2a296fafa
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-16 15:11:01 +0900

    feat: #32 - RxDataSource에서 사용할 SectionModel 및 DataSource 정의
    - 섹션은 헤더, 타입, 아이템으로 반환됨
    - 섹션은 검색할 키워드 칩,   카테고리에 따른 라벨 칩, 플랫폼 칩으로 나눌 수 있음
    - 각각의 셀과 헤더 타입 반환
    - 헤더의 경우 세그먼트 컨트롤의 이벤트를 방출해야하기 떄문에 바인딩 코드 추가

--- BestWish/Presentation/Scene/Anlaysis/ViewController/AnalaysisViewController.swift
+++ BestWish/Presentation/Scene/Anlaysis/ViewController/AnalaysisViewController.swift
@@ -11,6 +11,69 @@ import UIKit
 final class AnalaysisViewController: UIViewController {
     
     private let analysisView = AnalysisView()
+    private let viewModel: AnalysisViewModel
+    private var setHeaderView = false
+    var disposeBag = DisposeBag()
+    var previouslySelectedPlatformIndexPath: IndexPath?
+    
+    // 데이터 소스
+    lazy var dataSource = RxCollectionViewSectionedReloadDataSource<AnalysisSectionModel>(
+            configureCell: { (dataSource, collectionView, indexPath, item) in
+                switch item {
+                case let .keyword(keyword):
+                    guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: KeywordCell.identifier,
+                        for: indexPath
+                    ) as? KeywordCell else { return UICollectionViewCell() }
+                    cell.configure(keyword: keyword)
+                    return cell
+                    
+                case .attribute(let attribute, let isSelected):
+                    guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: AttributeCell.identifier,
+                        for: indexPath
+                    ) as? AttributeCell else { return UICollectionViewCell() }
+                    cell.configure(attribute: attribute, isSelected: isSelected)
+                    return cell
+                    
+                case let .platform(platform, isSelected):
+                    guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: PlatformCell.identifier,
+                        for: indexPath
+                    ) as? PlatformCell else { return UICollectionViewCell() }
+                    cell.configure(type: platform, isSelected: isSelected)
+                    return cell
+                }
+            }, configureSupplementaryView: { dataSource, collectionView, kind, indexPath in
+                guard indexPath.section == 1 else { return UICollectionReusableView() }
+                guard let headerView = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: kind,
+                    withReuseIdentifier: SegmentControlHeaderView.identifier,
+                    for: indexPath
+                ) as? SegmentControlHeaderView else {
+                    return UICollectionReusableView()
+                }
+                
+                headerView.getClassSegmentControl.rx.selectedSegmentIndex
+                    .distinctUntilChanged()
+                    .filter { $0 != UISegmentedControl.noSegment } // prevent -1 crash
+                    .bind(with: self) { owner, index in
+                        let category = headerView.getClassSegmentControl.titleForSegment(at: index) ?? ""
+                        owner.viewModel.action.onNext(.currentString(category))
+                    }
+                    .disposed(by: headerView.disposeBag)
+                return headerView
+            })
+    
+
+    init(viewModel: AnalysisViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = analysisView
