37aabe179a039a8ab69a8158f17456a774d5f581
LeeSuHyeon <tngus0673@naver.com>
2025-06-17 10:42:38 +0900

    feature: #73 -위시 리스트 조회 로직 수정
    1. 위시 리스트 아이템 조회 날짜 순 정렬
    2. 위시리스트 포함된 플랫폼 가져오기, 개수까지 반환

--- BestWish/Data/Network/Manager/Supabase/SupabaseManager.swift
+++ BestWish/Data/Network/Manager/Supabase/SupabaseManager.swift
@@ -47,14 +47,29 @@ final class SupabaseManager {
     }
 
     // 위시 리스트에 포함된 플랫폼 조회
-    func getPlatformsInWishList() async throws -> [PlatformDTO] {
+    func getPlatformsInWishList(userInfo: UserDTO) async throws -> [(platform: Int, count: Int)] {
+        let sequence = userInfo.platformSequence ?? []
         do {
-            return try await supabase
+            // product 테이블에서 사용자와 일치하는 platform들만 가져오기
+            let result: [PlatformDTO] = try await supabase
                 .from(SupabaseTable.product.rawValue)
                 .select(ProductAttributes.platform.rawValue)
                 .eq(ProductAttributes.user_id.rawValue, value: supabase.auth.session.user.id)
                 .execute()
                 .value
+            let platforms = result.map { Int($0.platform) }
+
+            let platformCountDict = platforms.reduce(into: [:]) { result, platform in
+                result[platform, default: 0] += 1
+            }
+
+            // sequence 순서에 따라 정렬 및 매핑
+            let sortedResult = sequence.compactMap { platform -> (platform: Int, count: Int)? in
+                guard let count = platformCountDict[platform] else { return nil }
+                return (platform: platform, count: count)
+            }
+
+            return sortedResult
         } catch {
             throw SupabaseError.selectError(error)
         }
@@ -78,6 +93,7 @@ final class SupabaseManager {
             }
 
             return try await builder
+                .order(ProductAttributes.created_at.rawValue, ascending: false)
                 .execute()
                 .value
         } catch {
@@ -112,7 +128,8 @@ final class SupabaseManager {
                 discountRate: product.discountRate,
                 brand: product.brand,
                 imagePathURL: product.imagePathURL,
-                productURL: product.productURL
+                productURL: product.productURL,
+                createdAt: nil
             )
 
             try await supabase
