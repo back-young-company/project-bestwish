7af9eb7ca9283a84a83bd006e35ecc66e864bd14
LeeSuHyeon <tngus0673@naver.com>
2025-06-24 19:21:28 +0900

    test: #121 - MockuserInfoUseCase 메서드 추가

--- BestWishTests/MyPage/Mock/MockUserInfoUseCase.swift
+++ BestWishTests/MyPage/Mock/MockUserInfoUseCase.swift
@@ -25,7 +25,25 @@ final class MockUserInfoUseCase: UserInfoUseCase {
 
         return user!
     }
-    
+
+    func updateUserInfo(profileImageCode: Int?, nickname: String?, gender: Int?, birth: Date?) async throws {
+        if shouldThrow {
+            throw AppError.supabaseError(
+                SupabaseError.selectError("updateUserInfo Error")
+            )
+        }
+        user = User(
+            name: nil,
+            email: "test",
+            nickname: nickname,
+            gender: gender,
+            birth: birth,
+            profileImageCode: profileImageCode ?? Int.min,
+            authProvider: "test"
+        )
+        return
+    }
+
     func isValidNickname(_ nickname: String) -> Bool {
         true
     }
