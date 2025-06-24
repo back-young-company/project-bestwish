c6be72f15b96e03ec022df075cb4225b72f1f805
LeeSuHyeon <tngus0673@naver.com>
2025-06-24 20:23:38 +0900

    test: #121 - 유저 정보 업데이트 뷰 모델 디버깅용 extension 추가

--- BestWish/Presentation/Scene/MyPage/UserInfoUpdate/ViewModel/UserInfoUpdateViewModel.swift
+++ BestWish/Presentation/Scene/MyPage/UserInfoUpdate/ViewModel/UserInfoUpdateViewModel.swift
@@ -133,3 +133,13 @@ final class UserInfoUpdateViewModel: ViewModel {
         }
     }
 }
+
+// MARK: - 테스트 전용 메서드
+#if DEBUG
+extension UserInfoUpdateViewModel {
+    /// 유저 정보 초기값 설정
+    func injectIntialUserInfo(_ user: UserInfoModel) {
+        _userInfo.accept(user)
+    }
+}
+#endif
