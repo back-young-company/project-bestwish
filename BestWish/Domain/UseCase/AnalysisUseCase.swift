35ffa7f54a26403e46fe73228b32589aa5278547
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-26 09:31:19 +0900

    refactor: #130 - Domain Layer 관련
    - Domain에서 UIkit을 import하고 있던 문제 모두 해결
    - LabelData -> LabelDataEntity
    - 섹션모델을 UseCase에서 알고 있던 문제 해결 기본 타입 혹은 Entity만 알고 있도록 수정
    - UsecaseImpl 로직 수정

--- BestWish/Domain/UseCase/AnalysisUseCase.swift
+++ BestWish/Domain/UseCase/AnalysisUseCase.swift
@@ -8,75 +8,39 @@
 /// 이미지 분석 유즈 케이스
 protocol AnalysisUseCase {
     /// 키워드 추가 이벤트
-    func addKeyword(_ keyword: String, models: [AnalysisSectionModel]) -> [AnalysisSectionModel]
+    func addKeyword(_ keyword: String, keywords: inout [String]) -> [String]
     /// 키워드 삭제 이벤트
-    func deleteKeyword(_ keyword: String, models: [AnalysisSectionModel]) -> [AnalysisSectionModel]
-    /// 플랫폼 선택 이벤트
-    func selectePlatform(_ platform: PlatformItem, models: [AnalysisSectionModel]) -> [AnalysisSectionModel]
-    /// 초기화
-    func resetKeyword(models: [AnalysisSectionModel]) -> [AnalysisSectionModel]
+    func deleteKeyword(_ keyword: String, keywords: inout [String]) -> [String]
     /// 플랫폼 이동
-    func movePlatform(platform: PlatformItem?) throws -> String
-    /// 속성 버튼 세팅
-    func setAttributeButton(_ item: AnalysisItem, keyword: String, selectedPlatform: PlatformItem?, isSelected: Bool) -> AnalysisItem
+    func movePlatform(deepLink: String?) throws -> String
 }
 
 /// 이미지 분석 유즈 케이스 구현체
 final class AnalysisUseCaseImpl: AnalysisUseCase {
     
     /// 키워드 추가 이벤트
-    func addKeyword(_ keyword: String, models: [AnalysisSectionModel]) -> [AnalysisSectionModel] {
-        var models = models
-        if !models[0].items.contains(.keyword(keyword: keyword)) {
-            models[0].items.append(.keyword(keyword: keyword))
+    func addKeyword(_ keyword: String, keywords: inout [String]) -> [String] {
+        if !keywords.contains(keyword) {
+            keywords.append(keyword)
         }
-        models[1].items = models[1].items.map { setAttributeButton($0, keyword: keyword, isSelected: true) }
-        return models
+        return keywords
     }
     
     /// 키워드 삭제 이벤트
-    func deleteKeyword(_ keyword: String, models: [AnalysisSectionModel]) -> [AnalysisSectionModel] {
-        var models = models
-        models[0].items.removeAll(where: { $0 == .keyword(keyword: keyword) })
-        models[1].items = models[1].items.map { setAttributeButton($0, keyword: keyword) }
-        return models
-    }
-    
-    /// 플랫폼 선택 이벤트
-    func selectePlatform(_ platform: PlatformItem, models: [AnalysisSectionModel]) -> [AnalysisSectionModel] {
-        var models = models
-        models[2].items = models[2].items.map { setAttributeButton($0, selectedPlatform: platform) }
-        return models
-    }
-    
-    /// 초기화
-    func resetKeyword(models: [AnalysisSectionModel]) -> [AnalysisSectionModel] {
-        var models = models
-        models[0] = AnalysisSectionModel(header: nil, type: .keyword, items: [])
-        return models
+    func deleteKeyword(_ keyword: String, keywords: inout [String]) -> [String] {
+        keywords.removeAll(where: { $0 == keyword })
+        return keywords
     }
     
     /// 플랫폼 이동
-    func movePlatform(platform: PlatformItem?) throws -> String {
-        guard let link = platform?.platformDeepLink else {
+    func movePlatform(deepLink: String?) throws -> String {
+        guard let deepLink else {
             throw PlatformError.notFoundDeepLink
         }
-        if link != "notFound" {
-            return link
+        if deepLink != "notFound" {
+            return deepLink
         } else {
             throw PlatformError.preparePlatform
-        }
-    }
-    
-    /// 속성 버튼 세팅
-    func setAttributeButton(_ item: AnalysisItem, keyword: String = "", selectedPlatform: PlatformItem? = nil, isSelected: Bool = false) -> AnalysisItem {
-        switch item {
-        case let .attribute(attr, _) where attr == keyword:
-            return .attribute(attribute: attr, isSelected: isSelected)
-        case let .platform(platform, _):
-            return .platform(platform: platform, isSelected: selectedPlatform?.platformName == platform.platformName)
-        default:
-            return item
         }
     }
 }
