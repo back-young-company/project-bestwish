77ea419dc4c401383fbe3ca99f1e45d00456933e
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-27 12:21:02 +0900

    feat: #162 - KreamFetcher 정의

---
+++ BestWish/Data/Network/Fetcher/KreamFetcher.swift
@@ -0,0 +1,17 @@
+//
+//  KreamFetcher.swift
+//  BestWish
+//
+//  Created by Quarang on 6/27/25.
+//
+
+import Foundation
+
+/// 크림 페쳐
+final class KreamFetcher: ProductDTORepository {
+    
+    /// 크림 URL -> DTO
+    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
+        <#code#>
+    }
+}
