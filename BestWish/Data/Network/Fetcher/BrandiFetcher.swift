d634aaedc4a3a833f2bc4b7c53df53352d8432a3
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-27 17:46:17 +0900

    feat: #162 - 브랜디 Fetcher 구현
    - 각각의 필드 html로 파싱 후 DTO로 변환

--- BestWish/Data/Network/Fetcher/BrandiFetcher.swift
+++ BestWish/Data/Network/Fetcher/BrandiFetcher.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftSoup
+
+/// 브랜디 Fetcher
+final class BrandiFetcher: ProductDTORepository {
+    
+    /// 브랜디 제품 상세 HTML → ProductDTO 파싱
+    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
+        let doc = try SwiftSoup.parse(html ?? "")
+        
+        let title = try doc.title()
+        let imageURL = try doc.select("meta[property=og:image]").attr("content")
+        let brand = ((((try? JSONSerialization.jsonObject(with: (try doc.select("script#prefetch-data").first()?.html().data(using: .utf8) ?? Data()))) as? [String: Any])?["data"] as? [String: Any])?["seller"] as? [String: Any])?["name"] as? String ?? ""
+        let discountRate = ((try? JSONSerialization.jsonObject(with: SwiftSoup.parse(html ?? "").select("script#prefetch-data").first()!.html().data(using: .utf8)!) as? [String: Any])?["data"] as? [String: Any])?["sale_percent"] as? Int ?? 0
+        let price = Int(
+            try doc.select("meta[property=og:title]").attr("content")
+                .components(separatedBy: CharacterSet.decimalDigits.inverted)
+                .joined()
+        )
+        
+        return ProductDTO(
+            id: nil,
+            userID: nil,
+            platform: 5,
+            title: title,
+            price: price,
+            discountRate: "\(discountRate)",
+            brand: brand,
+            imagePathURL: imageURL,
+            productURL: ogUrl?.absoluteString,
+            createdAt: nil
+        )
+    }
+}
+
+// https://www.brandi.co.kr/products/174895988?search-word=%EB%B9%84%ED%82%A4%EB%8B%88
