d466ed8c5cd829ca4e42185661f73560570f654a
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-27 15:05:45 +0900

    feat: #162 - KreamFetcher 구현
    - 해당 상품의 html을 파싱해서 DTO를 만들어 반환

--- BestWish/Data/Network/Fetcher/KreamFetcher.swift
+++ BestWish/Data/Network/Fetcher/KreamFetcher.swift
@@ -6,12 +6,35 @@
 //
 
 import Foundation
+import SwiftSoup
 
 /// 크림 페쳐
 final class KreamFetcher: ProductDTORepository {
     
-    /// 크림 URL -> DTO
+    /// 크림 제품 상세 HTML → ProductDTO 파싱
     func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
-        <#code#>
+        let doc = try SwiftSoup.parse(html ?? "")
+
+        let title = try doc.select("p.sub-title").first()?.text() ?? ""
+        let imageURL = try doc.select("img.full_width.image").first()?.attr("abs:src") ?? ""
+        let priceText  = try doc.select("p.price").first()?.text() ?? ""
+        let price = Int(priceText.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression))
+        let brand = try doc.select("p.title-text").first()?.text()
+        
+        return ProductDTO(
+            id: nil,
+            userID: nil,
+            platform: 1,
+            title: title,
+            price: price,
+            discountRate: "0",
+            brand: brand,
+            imagePathURL: imageURL,
+            productURL: ogUrl?.absoluteString,
+            createdAt: nil
+        )
     }
 }
+
+
+// [KREAM] Asics x Cecilie Bahnsen Gel-Cumulus 16 Gray https://kream.co.kr/products/533996
