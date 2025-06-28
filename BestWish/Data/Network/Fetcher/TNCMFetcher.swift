2352b38ca395a9a00899a91a444d9e9b24e29a76
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-28 15:05:56 +0900

    feat: #162 - 29CM Fetacher 구현
    - 각각의 정규식으로 해당 상품 DTO 반환

--- BestWish/Data/Network/Fetcher/TNCMFetcher.swift
+++ BestWish/Data/Network/Fetcher/TNCMFetcher.swift
@@ -14,31 +14,49 @@ final class TNCMFetcher: ProductDTORepository {
     /// 29CM 제품 상세 HTML → ProductDTO 파싱
     func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
         let html = html ?? ""
-        let doc = try SwiftSoup.parse(html ?? "")
-        
-        print(doc.body())
+        let doc = try SwiftSoup.parse(html)
 
         let title = try doc.title()
         let imageURL = try doc.select("meta[property=og:image]").attr("content")
-        let price = try doc.select("script:contains(__next_f.push)").first()
-        let brand = try doc.select("p.title-text").first()?.text()
         let url = try doc.select("meta[property=al:ios:url]").attr("content")
+
+        // 가격 및 할인율 추출
+        guard let range = html.range(of: #"totalDiscountedItemPrice.*?\}"#, options: .regularExpression) else { throw ProductSyncError.jsonDecodingFailed }
+        let result = String(html[range])
         
         
-        print(ProductDTO(
+        let regex = try NSRegularExpression(pattern: "\\d+", options: [])
+        let matches = regex.matches(in: result, options: [], range: NSRange(location: 0, length: result.utf16.count))
+        let numbers = matches.compactMap { match -> Int? in
+            guard let range = Range(match.range, in: result) else { return nil }
+            return Int(result[range])
+        }
+        
+        // 브랜드 추출
+        guard let brand_range = html.range(of: #"brandNameKor.*?\,"#, options: .regularExpression) else { throw ProductSyncError.jsonDecodingFailed }
+        let brand_result = String(html[brand_range])
+        
+        let brand_regex = try NSRegularExpression(pattern: #"brandNameKor\\":\\"(.*?)\\""#)
+        guard let match = brand_regex.firstMatch(in: brand_result, options: [], range: NSRange(location: 0, length: brand_result.utf16.count)),
+              let range = Range(match.range(at: 1), in: brand_result) else { throw ProductSyncError.jsonDecodingFailed }
+        
+        // 초기화
+        let price = numbers[0]
+        let discountRate = "\(numbers[1])"
+        let brand = String(brand_result[range])
+        
+        return ProductDTO(
             id: nil,
             userID: nil,
             platform: 6,
             title: title,
-            price: 0,
-            discountRate: nil,
+            price: price,
+            discountRate: discountRate,
             brand: brand,
             imagePathURL: imageURL,
             productURL: url,
             createdAt: nil
-        ))
-    
-        throw ProductSyncError.invalidProductData
+        )
     }
 }
 
