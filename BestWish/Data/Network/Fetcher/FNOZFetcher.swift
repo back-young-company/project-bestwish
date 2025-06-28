75ffca196777b049c42956b370bc81a192abcec6
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-28 12:54:43 +0900

    feat: #162 - 4910패쳐 내용 구현
    - json 데이터 정규식으로 파싱

--- BestWish/Data/Network/Fetcher/FNOZFetcher.swift
+++ BestWish/Data/Network/Fetcher/FNOZFetcher.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import SwiftSoup
+
+/// 4910 페쳐
+final class FNOZFetcher: ProductDTORepository {
+    
+    /// 크림 제품 상세 HTML → ProductDTO 파싱
+    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
+        let html = html ?? ""
+        let doc = try SwiftSoup.parse(html)
+
+        let title = try doc.title()
+        let imageURL = try doc.select("img[alt='상품 썸네일'][data-nimg=fill]").first()?.attr("abs:src")
+        
+        
+        // 가격
+        let price = Int(
+            (html.range(
+                of: #""discounted_price"\s*:\s*(\d+)"#,
+                options: .regularExpression
+            )
+            .flatMap { String(html[$0]) } ?? "")
+            .replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        )
+
+        // 할인율
+        let discount = html.range(of: #"\"extra_discount\"\s*:\s*\{[^}]*?\"discount_rate\"\s*:\s*(\d+)"#, options: .regularExpression)
+            .flatMap { String(html[$0]) }?
+            .replacingOccurrences(of: "[^0-9]", with: "",
+                                 options: .regularExpression)
+        
+        // 브랜드
+        let brandKor = html.range(
+            of: #"(?s)"brand"\s*:\s*\{.*?"name"\s*:\s*\"([^\"]+)""#,
+            options: [.regularExpression]
+        )
+        .flatMap { String(html[$0]) }?
+        .replacingOccurrences(
+            of: #"(?s).*?"name"\s*:\s*"|""#,
+            with: "",
+            options: [.regularExpression]
+        )
+        
+        return ProductDTO(
+            id: nil,
+            userID: nil,
+            platform: 8,
+            title: title,
+            price: price,
+            discountRate: discount,
+            brand: brandKor,
+            imagePathURL: imageURL,
+            productURL: ogUrl?.absoluteString,
+            createdAt: nil
+        )
+    }
+}
+
+// https://4910.kr/goods/43182588
