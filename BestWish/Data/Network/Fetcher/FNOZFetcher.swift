f029bcb5bd58c2a261aeba119a111627caef31d4
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-30 16:23:26 +0900

    feat: #162 - FNOSFetcher 수정
    - SwiftSoup 사용

--- BestWish/Data/Network/Fetcher/FNOZFetcher.swift
+++ BestWish/Data/Network/Fetcher/FNOZFetcher.swift
@@ -6,61 +6,43 @@
 //
 
 import Foundation
+
 import SwiftSoup
 
 /// 4910 페쳐
 final class FNOZFetcher: ProductDTORepository {
-    
     /// 크림 제품 상세 HTML → ProductDTO 파싱
-    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
-        let html = html ?? ""
-        let doc = try SwiftSoup.parse(html)
+    func fetchProductDTO(deepLink: URL?, productURL: URL?, html: String?) async throws -> ProductDTO {
+        guard let html else {
+            throw ProductSyncError.htmlParsingFailed
+        }
+        do {
+            let doc = try SwiftSoup.parse(html)
 
-        let title = try doc.title()
-        let imageURL = try doc.select("img[alt='상품 썸네일'][data-nimg=fill]").first()?.attr("abs:src")
-        
-        let productId = ogUrl?.absoluteString.replacingOccurrences(of: "https://4910.kr/goods/", with: "") ?? ""
-        
-        // 가격
-        let price = Int(
-            (html.range(
-                of: #""discounted_price"\s*:\s*(\d+)"#,
-                options: .regularExpression
+            // SwiftSoup으로 파싱 헤더에 데이터가 있을 때만 가능
+            let title = try doc.title()
+            let imageURL = try doc.select("img[alt='상품 썸네일'][data-nimg=fill]").first()?.attr("abs:src")
+            
+            // JsonData 혹은 Body에 필요한 데이터가 있는 경우 추출
+            let price = html.htmlExtractValue(pattern: #""discounted_price"\s*:\s*(\d+)"#) { Int($0) }
+            let discount = html.htmlExtractValue(pattern: #"\"extra_discount\"\s*:\s*\{[^}]*?"discount_rate"\s*:\s*(\d+)"#) { $0 }
+            let brandKor = html.htmlExtractValue(pattern: #"(?s)"brand"\s*:\s*\{.*?"name"\s*:\s*"([^"]+)""#) { $0 }
+            
+            return ProductDTO(
+                id: nil,
+                userID: nil,
+                platform: 7,
+                title: title,
+                price: price,
+                discountRate: discount,
+                brand: brandKor,
+                imagePathURL: imageURL,
+                productURL: deepLink?.absoluteString,
+                createdAt: nil
             )
-            .flatMap { String(html[$0]) } ?? "")
-            .replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
-        )
-
-        // 할인율
-        let discount = html.range(of: #"\"extra_discount\"\s*:\s*\{[^}]*?\"discount_rate\"\s*:\s*(\d+)"#, options: .regularExpression)
-            .flatMap { String(html[$0]) }?
-            .replacingOccurrences(of: "[^0-9]", with: "",
-                                 options: .regularExpression)
-        
-        // 브랜드
-        let brandKor = html.range(
-            of: #"(?s)"brand"\s*:\s*\{.*?"name"\s*:\s*\"([^\"]+)""#,
-            options: [.regularExpression]
-        )
-        .flatMap { String(html[$0]) }?
-        .replacingOccurrences(
-            of: #"(?s).*?"name"\s*:\s*"|""#,
-            with: "",
-            options: [.regularExpression]
-        )
-        
-        return ProductDTO(
-            id: nil,
-            userID: nil,
-            platform: 8,
-            title: title,
-            price: price,
-            discountRate: discount,
-            brand: brandKor,
-            imagePathURL: imageURL,
-            productURL: "aglo://webview?url=https://4910.kr/goods/\(productId)",
-            createdAt: nil
-        )
+        } catch {
+            throw ProductSyncError.dataLoadingFailed
+        }
     }
 }
 
