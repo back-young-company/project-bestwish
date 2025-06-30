93271881c45ad519577ae34cf83044f69fd3110d
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-30 16:23:57 +0900

    feat: #162 - MusinsaFetcher 수정
    - SwiftSoup 사용

--- BestWish/Data/Network/Fetcher/MusinsaFetcher.swift
+++ BestWish/Data/Network/Fetcher/MusinsaFetcher.swift
@@ -7,56 +7,39 @@
 
 import Foundation
 
-import RxSwift
+import SwiftSoup
 
 /// 무신사 Fetcher
 final class MusinsaFetcher: ProductDTORepository {
     /// 상품 데이터 fetch
-    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
-        let (data, _) = try await URLSession.shared.data(from: finalUrl!)
-        guard let finalHTML = String(data: data, encoding: .utf8) else {
+    func fetchProductDTO(deepLink: URL?, productURL: URL?, html: String?) async throws -> ProductDTO {
+        guard let html else {
+            throw ProductSyncError.htmlParsingFailed
+        }
+        do {
+            let doc = try SwiftSoup.parse(html)
+            
+            // SwiftSoup으로 파싱 헤더에 데이터가 있을 때만 가능
+            let title = try doc.title()
+            let imageURL = try doc.select("meta[property=og:image]").attr("content")
+            let price = Int(try doc.select("meta[property=product:price:amount]").first()?.attr("content") ?? "")
+            let discountRate = try doc.select("meta[property=product:price:sale_rate]").first()?.attr("content")
+            let brand = try doc.select("meta[property=product:brand]").first()?.attr("content")
+            
+            return ProductDTO(
+                id: nil,
+                userID: nil,
+                platform: 3,
+                title: title,
+                price: price,
+                discountRate: discountRate,
+                brand: brand,
+                imagePathURL: imageURL,
+                productURL: deepLink?.absoluteString,
+                createdAt: nil
+            )
+        } catch {
             throw ProductSyncError.dataLoadingFailed
         }
-
-        // 무신사는 최초 공유 링크가 'https://musinsa.onelink.me/PvkC/8o9es6co' 형식
-        // 위 형식의 링크로 html 변환 후 JSON 파싱을 시도할 경우 실패
-        // firstMatch 메서드를 활용하여 html 내 'https://www.musinsa.com/products/2690691' 형식 링크 탐색
-        // 탐색 된 상품 링크로 redirectedURL 생성 및 finalUrl과 비교 진행
-        if let productURLString = finalHTML.firstMatch(for: #"link=(https:\/\/www\.musinsa\.com\/products\/\d+)"#),
-           let redirectedURL = URL(string: productURLString),
-           redirectedURL != finalUrl {
-            return try await fetchProductDTO(ogUrl: ogUrl, finalUrl: redirectedURL, html: finalHTML)
-        }
-
-        guard let json = finalHTML.extractNEXTDataJSON() else {
-            throw ProductSyncError.jsonScriptParsingFailed
-        }
-
-        let goodsNmPattern = #""goodsNm"\s*:\s*"([^"]+)""#
-        let thumbnailImageUrlPattern = #""thumbnailImageUrl"\s*:\s*"([^"]+)""#
-        let brandNamePattern = #""brandName"\s*:\s*"([^"]+)""#
-        let salePricePattern = #""salePrice"\s*:\s*"?([0-9]+)"?"#
-        let discountRatePattern = #""discountRate"\s*:\s*"?([0-9]+)"?"#
-
-        guard let goodsNm = json.firstMatch(for: goodsNmPattern),
-              let thumbnailImageUrl = json.firstMatch(for: thumbnailImageUrlPattern),
-              let brandName = json.firstMatch(for: brandNamePattern),
-              let salePrice = json.firstMatch(for: salePricePattern),
-              let discountRate = json.firstMatch(for: discountRatePattern) else {
-            throw ProductSyncError.invalidProductData
-        }
-
-        return ProductDTO(
-            id: nil,
-            userID: nil,
-            platform: 1,
-            title: goodsNm,
-            price: Int(salePrice),
-            discountRate: discountRate,
-            brand: brandName,
-            imagePathURL: "https://image.msscdn.net" + thumbnailImageUrl,
-            productURL: ogUrl?.absoluteString,
-            createdAt: nil
-        )
     }
 }
