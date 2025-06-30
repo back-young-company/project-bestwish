0a3578f57a462ad235299d0e44f34ac0ee3ee351
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-30 16:22:55 +0900

    feat: #162 - AblyFetcher 수정
    - SwiftSoup 사용

--- BestWish/Data/Network/Fetcher/AblyFetcher.swift
+++ BestWish/Data/Network/Fetcher/AblyFetcher.swift
@@ -7,32 +7,37 @@
 
 import Foundation
 
+import SwiftSoup
+
 /// 에이블리 Fetcher
 final class AblyFetcher: ProductDTORepository {
     /// 상품 데이터 fetch
-    func fetchProductDTO(ogUrl: URL?, finalUrl: URL?, html: String?) async throws -> ProductDTO {
-        guard let json = html?.extractNEXTDataJSON(),
-              let jsonData = json.data(using: .utf8) else {
-            throw ProductSyncError.jsonScriptParsingFailed
+    func fetchProductDTO(deepLink: URL?, productURL: URL?, html: String?) async throws -> ProductDTO {
+        guard let html else {
+            throw ProductSyncError.htmlParsingFailed
         }
-
         do {
-            let decoded = try JSONDecoder().decode(AblyResponseDTO.self, from: jsonData)
-            guard let goods = decoded.props.serverQueryClient.queries.first?.state.data.goods,
-                  let sno = goods.sno else {
-                throw ProductSyncError.invalidProductData
-            }
-
+            let doc = try SwiftSoup.parse(html)
+            
+            // SwiftSoup으로 파싱 헤더에 데이터가 있을 때만 가능
+            let title = try doc.title()
+            let imageURL = try doc.select("meta[property=og:image]").attr("content")
+            
+            // JsonData 혹은 Body에 필요한 데이터가 있는 경우 추추출
+            let price = html.htmlExtractValue(pattern: #""thumbnail_price"\s*:\s*(\d+)"#) { Int($0) }
+            let discountRate = html.htmlExtractValue(pattern: #""discount_rate"\s*:\s*(\d+)"#) { $0 }
+            let brand = try doc.select("p.typography.typography__subtitle2.color__gray70").first()?.text()
+            
             return ProductDTO(
                 id: nil,
                 userID: nil,
                 platform: 3,
-                title: goods.name,
-                price: goods.priceInfo.thumbnailPrice,
-                discountRate: String(goods.priceInfo.discountRate ?? 0),
-                brand: goods.market.name,
-                imagePathURL: goods.coverImages.first,
-                productURL: "ably://goods/\(sno)",
+                title: title,
+                price: price,
+                discountRate: discountRate,
+                brand: brand,
+                imagePathURL: imageURL,
+                productURL: deepLink?.absoluteString,
                 createdAt: nil
             )
         } catch {
